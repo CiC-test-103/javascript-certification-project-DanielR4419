d51c6c3137f12e689b5cb85bb641f132d65f4b85
DanielR4419 <daniel_rodas_f@hotmail.com>
2025-02-02 23:44:02 -0800

    Certification project done

--- LinkedList.js
+++ LinkedList.js
@@ -1,5 +1,6 @@
 // Necessary Imports (you will need to use this)
 const { Student } = require('./Student')
+const fs = require("fs/promises"); //for asynchronous code
 
 /**
  * Node Class (GIVEN, you will need to use this)
@@ -36,7 +37,9 @@ class LinkedList {
    * RETURNS:   None
    */
   constructor() {
-    // TODO
+    this.head = null; //Head initializes as null because it's an empty list
+    this.tail = null; //Tail also initializes as null
+    this.length = 0; //Begins with 0 and increments as nodes are added
   }
 
   /**
@@ -48,7 +51,15 @@ class LinkedList {
    * - Think about adding to the 'end' of the LinkedList (Hint: tail)
    */
   addStudent(newStudent) {
-    // TODO
+    let newNode = new Node(newStudent); //Create a new student
+    if(!this.head){ //If the list is empty, set head and tail to the new student
+      this.head = newNode;
+      this.tail = newNode;
+    } else{ //Otherwise, add the new student to the end and update the tail
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++; //Increase the length
   }
 
   /**
@@ -60,7 +71,35 @@ class LinkedList {
    * - Think about how removal might update head or tail
    */
   removeStudent(email) {
-    // TODO
+    if(!this.head){
+      return null; //For an empty list there will be no student to remove
+    }
+
+    //Case when the student removed is from the head of the list
+    if(this.head.data.getEmail() === email){ //Using method from Student object to find data by email
+      this.head = this.head.next; //move the head to the next student
+    if(!this.head){
+      this.tail = null; //If list is now empty we need update the tail to null
+    }
+    this.length--; //Decrease the length
+    return true; //True indicates the student was indeed removed
+  }
+
+    //Case when we remove a non-head student
+    let current = this.head; //Starting from the head of the list
+    while(current.next){
+      if(current.next.data.getEmail() === email){
+        //Checks if the email associated with the next node fits the email parameter
+        current.next = current.next.next; //If there is a match, the student is removed by updating the next pointer of the current student to skip the node to be removed
+        if(!current.next){
+          this.tail = current; //Updates the tail if the tail is the removed one
+        }
+        this.length--; //Decrease the length
+        return true; //Return true to indicate that a student was removed
+      }
+      current = current.next; //Go to next student in list
+    }
+    return false; //When student was not found by email, return false to indicate no student was removed
   }
 
   /**
@@ -69,8 +108,14 @@ class LinkedList {
    * RETURNS:   The Student or -1 if not found
    */
   findStudent(email) {
-    // TODO
-    return -1
+    let current = this.head; //Starts from the head of the list
+    while(current){
+      if(current.data.getEmail() === email){ //Checks if the current email matches the email parameter
+        return current.data; //displays the student data
+      }
+      current = current.next; //Moves to the next student in list
+    }
+    return -1 //Otherwise if student not found, returns -1
   }
 
   /**
@@ -78,8 +123,10 @@ class LinkedList {
    * EFFECTS:   Clears all students from the Linked List
    * RETURNS:   None
    */
-  #clearStudents() {
-    // TODO
+  clearStudents() { //Same information as empty list when declaring the constructor
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
   }
 
   /**
@@ -91,8 +138,13 @@ class LinkedList {
    *  - Output should appear as: "JohnDoe, JaneDoe"
    */
   displayStudents() {
-    // TODO
-    return "";
+    let current = this.head; //Starts from the head of the list
+    let result = []; //Store students in an array
+    while(current){
+      result.push(current.data.getName()); //Put students names in the array
+      current = current.next; //Move to the next student in list
+    }
+    return result.join(", "); //Join method converts result in a string, separating names with a comma and space
   }
 
   /**
@@ -100,9 +152,14 @@ class LinkedList {
    * EFFECTS:   None
    * RETURNS:   A sorted array of students by name
    */
-  #sortStudentsByName() {
-    // TODO
-    return [];
+  sortStudentsByName() {
+    const students = [];
+    let current = this.head;
+    while(current){
+      students.push(current.data);
+      current = current.next;
+    }
+    return students.sort((a, b) => a.getName().localeCompare(b.getName())); //Sort students alphabetically based on their names and compares them
   }
 
   /**
@@ -113,8 +170,15 @@ class LinkedList {
    * - Use sortStudentsByName()
    */
   filterBySpecialization(specialization) {
-    // TODO
-    return [];
+    const filteredStudents = [];
+    let current = this.head;
+    while(current){
+      if(current.data.getSpecialization() === specialization){
+        filteredStudents.push(current.data);
+      }
+      current = current.next;
+    }
+    return this.sortStudentsByName(filteredStudents);
   }
 
   /**
@@ -125,8 +189,15 @@ class LinkedList {
    * - Use sortStudentsByName()
    */
   filterByMinAge(minAge) {
-    // TODO
-    return [];
+    const filteredStudents = [];
+    let current = this.head;
+    while(current){
+      if(current.data.age === minAge){
+        filteredStudents.push(current.data);
+      }
+      current = current.next;
+    }
+    return this.sortStudentsByName(filteredStudents);
   }
 
   /**
@@ -135,8 +206,19 @@ class LinkedList {
    * RETURNS:   None
    */
   async saveToJson(fileName) {
-    // TODO
+    try{
+    const students = [];
+    let current = this.head;
+    while(current){
+      students.push(current.data);
+      current = current.next;
+    }
+    await fs.writeFile(fileName, JSON.stringify(students, null, 2)); //for readability and indentation
+    console.log(`Data successfully saved to ${fileName}`);
+  } catch(error){
+    console.log(`Error saving data to ${fileName}:`, error);
   }
+}
 
   /**
    * REQUIRES:  A valid file name (String) that exists
@@ -146,7 +228,15 @@ class LinkedList {
    *  - Use clearStudents() to perform overwriting
    */
   async loadFromJSON(fileName) {
-    // TODO
+    try{
+      const data = await fs.readFile(fileName, "utf-8");
+      const students = JSON.parse(data);
+      this.clearStudents();
+      students.forEach(student => this.addStudent(new Student(student))); //to iterate over each student in students array. For each student a new student is created using the student constructor. Add student method for adding a new student to the linked list.
+      console.log(`Data successfully saved to ${fileName}`);
+    } catch(error){
+      console.log(`Error saving data to ${fileName}:`, error);
+    }
   }
 
 }
--- index.js
+++ index.js
@@ -36,105 +36,77 @@ async function handleCommand(command) {
 
   switch (operation) {
     case 'add':
-      /**
-       * TODO:
-       *  Finds a particular student by email, and returns their information
-       *  You will need to do the following:
-       *   - Implement LinkedList (run tests locally to check implementation)
-       *   - Grab the args (code is given)
-       *   - Use implemented functions in LinkedList to add the Student, and display the updated LinkedList
-       */
         console.log('Adding student...')
-        const [name, year, email, specialization] = args
+        const [name, year, email, specialization] = args;
         // --------> WRITE YOUR CODE BELOW
-
+        //Create a new Student object
+        const newStudent = new Student(name, parseInt(year), email, specialization);
+        //Add student to the linked List
+        studentManagementSystem.add(newStudent);
+        //Display the updated linked list
+        studentManagementSystem.display();
         // --------> WRITE YOUR CODE ABOVE
         break;
 
     case 'remove':
-      /**
-       * TODO:
-       *  Removes a particular student by email
-       *  You will need to do the following:
-       *   - Implement LinkedList (run tests locally to check implementation)
-       *   - Grab the args (removeEmail)
-       *   - Use implemented functions in LinkedList to remove the Student, and display the updated LinkedList
-       */
       console.log('Removing student...')
       // --------> WRITE YOUR CODE BELOW
-      
+      const [removeEmail] = args;
+      //Remove the student by email
+      studentManagementSystem.removeByEmail(removeEmail);
+      //Display the updated linked list
+      studentManagementSystem.display();
       // --------> WRITE YOUR CODE ABOVE
       break;
 
     case 'display':
-      /**
-       * TODO:
-       *  Displays the students in the Linked List
-       *  You will need to do the following:
-       *   - Use implemneted functions in LinkedList to display the student
-       */
       console.log('Displaying students...')
       // --------> WRITE YOUR CODE BELOW
-
+      //Display all students in linked list
+      studentManagementSystem.display();
       // --------> WRITE YOUR CODE ABOVE
       break;
 
     case 'find':
-      /**
-       * TODO:
-       *  Finds a particular student by email, and returns their information
-       *  You will need to do the following:
-       *   - Implement LinkedList (run tests locally to check implementation)
-       *   - Grab the args (findEmail)
-       *   - Use implemented functions in LinkedList to grab the Student
-       *   - Use implemented functions in Student to display if found, otherwise, state "Student does not exist"
-       */
       console.log('Finding student...')
       // --------> WRITE YOUR CODE BELOW
-      
+      const [findEmail] = args;
+      //Find student by email
+      const foundStudent = studentManagementSystem.findByEmail(findEmail);
+      if(foundStudent){
+        console.log("Student found: ", foundStudent);
+      } else{
+        console.log("Student not found.");
+      }
       // --------> WRITE YOUR CODE ABOVE
       break;
 
     case 'save':
-      /**
-       * TODO:
-       *  Saves the current LinkedList to a specified JSON file
-       *  You will need to do the following:
-       *   - Implement LinkedList (run tests locally to check implementation)
-       *   - Grab the args (saveFileName)
-       *   - Use implemented functions in LinkedList to save the data
-       */
       console.log('Saving data...')
       // --------> WRITE YOUR CODE BELOW
-
+      const [saveFileName] = args;
+      //Save linked list to a file
+      studentManagementSystem.saveToFile(saveFileName);
+      console.log(`Data saved to ${saveFileName}`);
       // --------> WRITE YOUR CODE ABOVE
 
     case "load":
-      /**
-       * TODO:
-       *  Loads data from specified JSON file into current Linked List
-       *  You will need to do the following:
-       *   - Implement LinkedList (run tests locally to check implementation)
-       *   - Grab the args (loadFileName)
-       *   - Use implemented functions in LinkedList to save the data, and display the updated LinkedList
-       */
       console.log('Loading data...')
       // --------> WRITE YOUR CODE BELOW
-
+      const [loadFileName] = args;
+      //Load the linked list from a file
+      studentManagementSystem.loadFromFile(loadFileName);
+      //Display all students in linked list
+      studentManagementSystem.display();
       // --------> WRITE YOUR CODE ABOVE
       break;
 
     case 'clear':
-      /**
-       * TODO:
-       *  Clears all data in the Linked List
-       *  You will need to do the following:
-       *   - Implement LinkedList (run tests locally to check implementation)
-       *   - Use implemented functions in LinkedList to clear the data
-       */
       console.log('Clearing data...')
       // --------> WRITE YOUR CODE BELOW
-
+      //Clear the linked list
+      studentManagementSystem.clear();
+      console.log("All data cleared.");
       // --------> WRITE YOUR CODE ABOVE
       break;
 
